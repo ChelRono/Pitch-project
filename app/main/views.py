9625beded4f2e2c23b9c80f4e262455012b96b6a
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-05-12 06:27:28 -0400

    added profile page

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from flask_login import login_required
+from ..models import  User
 
 # Views
 @main.route('/')
@@ -14,10 +15,14 @@ def index():
     return render_template('index.html',message = message)
 
 
-@main.route('/profile')
-def profile():
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
 
-	return render_template('profile.html')
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
 
 
 # @main.route('/pitches')
