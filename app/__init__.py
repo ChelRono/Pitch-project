584e03827d29a6c3989a0dfbadeaf7ac4bae910e
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-05-12 07:32:29 -0400

    initialized email

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
+from app import app as app
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from .main import main as main_blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from app import app as app
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 
@@ -49,6 +49,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     # Registering the blueprint
    
